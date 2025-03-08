a51a30e0594fc3d8f71c24ff87c8366a82bb1c26
Christian <60242560+Christian-Downs@users.noreply.github.com>
2025-03-07 23:34:36 -0600

    Starting calling function

---
+++ caller.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+client = OpenAI()
+
+
+
+def caller(prompt, system = None):
+    response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": "Hello"
+            }
+        ]
+        },
+        {
+        "role": "assistant",
+        "content": [
+            {
+            "type": "text",
+            "text": "Hello! How can I assist you today?"
+            }
+        ]
+        }
+    ],
+    response_format={
+        "type": "text"
+    },
+    temperature=1,
+    max_completion_tokens=2048,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+
+
