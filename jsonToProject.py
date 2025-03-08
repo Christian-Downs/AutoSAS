f8bbd24b58565773bb5f73f5b640333c0a2dbf84
Starship-Nova <ming.huang.mat@gmail.com>
2025-03-07 23:04:06 -0600

    add json extractor

--- jsonToProject.py
+++ jsonToProject.py
@@ -5,5 +5,40 @@ def jsonToProject (nameAndContent):
         with open(filename, 'w') as file:
             file.write(nameAndContentObject[filename])
 
+def jsonFromResponse (response) :
+    jsonBegin = False
+    jsonEnd = True
+
+    json = ""
+    for char in response :
+        if char == "}" :
+            jsonEnd = True
+        
+        if jsonBegin == True and jsonEnd == False :
+            json += char
+
+        if char == "{" :
+            jsonBegin = True
+            jsonEnd = False
+    
+    return json
+
+def jsonFromResponse (response) :
+    record = False
+
+    json = ""
+    for char in response :
+        if char == "{" :
+            record = True
+        
+        if record == True:
+            json += char
+
+        if char == "}" :
+            record = False
+    
+    return json
 
 jsonToProject(" {\"file1\" : \"file1 contents\" , \"file2\" : \"file2 contents\"} ")
+
+print(jsonFromResponse(" {\"file1\" : \"file1 contents\" , \"file2\" : \"file2 contents\"} "))
