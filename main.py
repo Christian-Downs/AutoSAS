40f53409e8a9fcf0118a987106fb38c91dd4f35d
Christian <60242560+Christian-Downs@users.noreply.github.com>
2025-03-07 21:48:31 -0600

    Add changes

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from Main2 import mainFunc2
+from flask import Flask
+
+
+app = Flask(__name__)
 
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+
+@app.route("/")
+def main():
+    return "HELLO"
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
