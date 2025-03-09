16ecd888b39faf52da79ccae86cdf6b94717b0c8
Christian Daniels <blockhead0500@gmail.com>
2025-03-08 18:06:25 -0600

    added testing call to aiTester

---
+++ aiTester.py
@@ -0,0 +1,33 @@
+from openai import OpenAI
+client = OpenAI()
+def caller(prompt) -> None:
+
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    store=True,
+    messages=[
+        {"role": "user", "content": "create website using python flask main.py using input "
+        + "Give file structure, file code, README.md Dont explain running input or overview:"
+        + prompt}
+    ]
+    )
+    # print(completion.choices[0].message.content)
+
+    with open('input.txt', 'w', encoding="utf-8") as file:
+        # file.write("\n\n---------Chat Reponse-----------")
+        file.write(str(completion.choices[0].message.content))
+        # file.write("\n-----------End of reponse-----------\n")
+
+def tester(filepath, error_code) -> None:
+    with open(filepath, 'r', encoding="utf-8") as file:
+        file_content = file.read()
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    store=True,
+    messages=[
+        {"role": "user", "content": "create website using python flask main.py using input "
+        + "Error in\n" + file_content + "\n" + error_code + "give updated code no explanation:"}
+    ]    
+    )
+    with open('outputTest.txt', 'w', encoding="utf-8") as file:
+        file.write(str(completion.choices[0].message.content))
