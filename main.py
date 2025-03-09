616ecd2717bf4d4321259486dc623948de92a1c3
Christian Daniels <blockhead0500@gmail.com>
2025-03-08 20:22:07 -0600

    Updated main and added comments

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 from flask import Flask, render_template, request
-import fileNameParse
-import jsonToProject
+from aiTester import caller
+from fileNameParse import convert_text_to_json
+from jsonToProject import jsonToProject
+import fileZipOutput
 
 
-
+#Render webpage
 app = Flask(__name__)
-
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 @app.route('/')
 def my_form():
     return render_template('my-form.html')
 
+#Send user input and execute code
 @app.route('/', methods=['POST'])
 def my_form_post():
+    # Example Usage:
     text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
 
-# Press the green button in the gutter to run the script.
+    #Send ChatGPT user prompt and store output input.txt
+    caller(text)
+    file_path = 'input.txt'
+    with open(file_path, 'r') as file:
+            file_content = file.read()
+            file.close()
+
+    #conver input.txt to .json file
+    with open("Output.json", "w") as file:
+        json_data = convert_text_to_json(file_content)
+        file.write(json_data)
+
+    #convert .json to files
+    jsonToProject(str(json_data))
+
+    #convert files to zip and display back to user
+    fileZipOutput.folder_to_zip("output")
+    return fileZipOutput.send_to_user()
+    
+#main call
 if __name__ == '__main__':
     app.run(debug=True)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
