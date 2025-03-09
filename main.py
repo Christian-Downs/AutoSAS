6397ab95176213502c3edd2c5cbf19afb00a5fe2
Starship-Nova <ming.huang.mat@gmail.com>
2025-03-09 00:20:18 -0600

    finalize main.py

--- main.py
+++ main.py
@@ -11,66 +11,6 @@ app = Flask(__name__)
 def my_form():
     return render_template('my-form.html')
 
-def jsonFromResponse (response) :
-    openBraceCount = 0
-
-    json = ""
-    for char in response :
-        if char == "{" :
-            openBraceCount += 1
-        
-        if openBraceCount > 0:
-            json += char
-
-        if char == "}" :
-            openBraceCount -= 1
-    
-    return json
-
-def templateJsonToPathJson (badJson) :
-    badDict = json.loads(badJson)
-    keys = list(badDict.keys())
-    fileTemplate = "\"" + badDict[keys[0]] + "\""
-
-    goodJson = "{"
-
-    fileJson = json.loads(fileTemplate)
-    fileList = list(fileJson.keys())
-    folders = [""]
-    folderDepth = 0
-    for name in fileList : 
-        nameList = name.split()
-        if "." in name :
-            if (folderDepth > nameList - 1) :
-                while folderDepth > nameList- 1 :
-                    folderDepth -= 1
-                    folders.pop()
-            filepath = ""
-            for folder in folders :
-                filepath += folder + "/"
-            filepath += nameList.pop()            
-            
-            goodJson += "\"" + filepath + "\":" + "\"" + fileJson[name] + "\""
-        elif nameList.size() == 1:
-            continue
-        else :
-            folders.append(nameList.pop())
-            folderDepth += 1
-    goodJson += "}"
-    print(goodJson)
-    return goodJson
-        
-"""
-{'employee_app': {'': None},
- '│': None,
- '├── main.py': None,
- '├── templates': {'': None}, 
- '│   ├── index.html': None,
- '│   └── employee_list.html': None,
- '└── README.md': None
-}
-"""
-
 @app.route('/', methods=['POST'])
 def my_form_post():
     # Example Usage:
@@ -83,9 +23,9 @@ def my_form_post():
         file.close()
     with open("Output.json", "w") as file:
         json_data = convert_text_to_json(file_content)
-        print(json_data)
-        json_data = templateJsonToPathJson(str(json_data))
+        
         file.write(str(json_data))
+        #json_data = templateJsonToPathJson(str(json_data))
     jsonToProject(str(json_data))
 
     return "PROCESSED"
